9020b7f6a2d4fa4fdefba24b4bc22726839534f5
ruwini01 <12345tharanga12345@gmail.com>
2025-10-18 08:13:37 +0530

    add next parameter to async controller functions for centralized error handling

--- backend/src/application/hotel.js
+++ backend/src/application/hotel.js
@@ -1,46 +1,50 @@
 const Hotel = require('../infrastructure/entities/Hotel');
+const NotFoundError = require("../domain/errors/not-found-error.js");
+const ValidationError = require("../domain/errors/validation-error.js");
 
-const getAllHotels = async (req, res) => {
-  const hotels = await Hotel.find();
+ const getAllHotels = async (req, res, next) => {
   try {
+    const hotels = await Hotel.find();
     res.status(200).json(hotels);
-  }
-  catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-}
-
-const getHotelById = async (req, res) => {
-  try {
-    const _id = req.params._id;
-    const hotel = await Hotel.findById(_id);
-    if (!hotel) {
-      res.status(404).send();
-      return;
-    }
-    res.status(200).json(hotel);
+    return;
   } catch (error) {
-    res.status(500).send();
+    next(error);
   }
 };
 
-
-const createHotel = async (req, res) => {
+ const createHotel = async (req, res, next) => {
   try {
     const hotelData = req.body;
-    if (!hotelData.name || !hotelData.image || !hotelData.location || !hotelData.price || !hotelData.description) {
-      res.status(400).json({ message: "Missing required fields" });
-      return;
+    if (
+      !hotelData.name ||
+      !hotelData.image ||
+      !hotelData.location ||
+      !hotelData.price ||
+      !hotelData.description
+    ) {
+      throw new ValidationError("Invalid hotel data");
     }
     await Hotel.create(hotelData);
     res.status(201).send();
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    next(error);
   }
 };
 
+ const getHotelById = async (req, res, next) => {
+  try {
+    const _id = req.params._id;
+    const hotel = await Hotel.findById(_id);
+    if (!hotel) {
+      throw new NotFoundError("Hotel not found");
+    }
+    res.status(200).json(hotel);
+  } catch (error) {
+    next(error);
+  }
+};
 
-const updateHotel = async (req, res) => {
+ const updateHotel = async (req, res, next) => {
   try {
     const _id = req.params._id;
     const hotelData = req.body;
@@ -51,56 +55,50 @@ const updateHotel = async (req, res) => {
       !hotelData.price ||
       !hotelData.description
     ) {
-      res.status(400).send();
-      return;
+      throw new ValidationError("Invalid hotel data");
     }
 
     const hotel = await Hotel.findById(_id);
     if (!hotel) {
-      res.status(404).send();
-      return;
+      throw new NotFoundError("Hotel not found");
     }
 
     await Hotel.findByIdAndUpdate(_id, hotelData);
-    res.status(200).send;
+    res.status(200).json(hotelData);
   } catch (error) {
-    res.status(500).send();
+    next(error);
   }
 };
 
-
-const patchHotel = async (req, res) => {
+ const patchHotel = async (req, res, next) => {
   try {
     const _id = req.params._id;
     const hotelData = req.body;
     if (!hotelData.price) {
-      res.status(400).send();
-      return;
+      throw new ValidationError("Price is required");
     }
     const hotel = await Hotel.findById(_id);
     if (!hotel) {
-      res.status(404).send();
-      return;
+      throw new NotFoundError("Hotel not found");
     }
     await Hotel.findByIdAndUpdate(_id, { price: hotelData.price });
     res.status(200).send();
   } catch (error) {
-    res.status(500).send();
+    next(error);
   }
 };
 
-const deleteHotel = async (req, res) => {
+ const deleteHotel = async (req, res, next) => {
   try {
     const _id = req.params._id;
     const hotel = await Hotel.findById(_id);
     if (!hotel) {
-      res.status(404).send();
-      return;
+      throw new NotFoundError("Hotel not found");
     }
     await Hotel.findByIdAndDelete(_id);
     res.status(200).send();
   } catch (error) {
-    res.status(500).send();
+    next(error);
   }
 };
 
