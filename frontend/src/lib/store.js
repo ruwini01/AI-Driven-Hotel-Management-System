db7da4dc33ded9e485854dd83e1329d784ac79b1
ruwini01 <12345tharanga12345@gmail.com>
2025-10-18 08:24:11 +0530

    configure Redux store with counter slice and RTK Query API middleware

---
+++ frontend/src/lib/store.js
@@ -0,0 +1,17 @@
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./features/counterSlice";
+import { api } from "./api";
+import { setupListeners } from "@reduxjs/toolkit/query";
+
+export const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+    [api.reducerPath]: api.reducer,
+  },
+  // Adding the api middleware enables caching, invalidation, polling,
+  // and other useful features of `rtk-query`.
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware),
+});
+
+setupListeners(store.dispatch)
