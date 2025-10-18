912672fafc9895dfed71b7f173195bbd7f384384
ruwini01 <12345tharanga12345@gmail.com>
2025-10-18 08:06:46 +0530

    middleware added for error handling, integrated with Clerk

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const connectDB = require('./src/infrastructure/db');
+const cors = require('cors');
+const globalErrorHandlingMiddleware = require('./src/api/middleware/global-error-handling-middleware');
+
+const { clerkMiddleware } = require('@clerk/express');
 
 //Connect to database
 connectDB();
@@ -10,6 +14,14 @@ const PORT =  8000;
 //Convert http payloads to json objects
 app.use(express.json());
 
+//Enable CORS for all routes
+app.use(cors({
+    origin: 'http://localhost:5173' 
+}
+));
+
+app.use(clerkMiddleware());
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
@@ -17,6 +29,11 @@ app.listen(PORT, () => {
 
 const hotelRoute = require('./src/api/hotel');
 const reviewRoute = require("./src/api/review");
+const locationRoute = require("./src/api/location");
 
 app.use('/api/hotels', hotelRoute);
 app.use("/api/reviews", reviewRoute);
+app.use("/api/locations", locationRoute);
+
+
+app.use(globalErrorHandlingMiddleware)
