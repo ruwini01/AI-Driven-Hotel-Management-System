e8dbe67f121beb7cf88d83e21aa36a331a1adcb3
ruwini01 <12345tharanga12345@gmail.com>
2025-10-18 08:17:21 +0530

    Location entity created

---
+++ backend/src/infrastructure/entities/Location.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const locationSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  }
+});
+
+const Location = mongoose.model("Location", locationSchema);
+
+module.exports = Location;
