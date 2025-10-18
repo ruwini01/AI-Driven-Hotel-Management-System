11e790d7a2131eb9d07cc162932b6312a9c828d8
ruwini01 <12345tharanga12345@gmail.com>
2025-10-18 08:21:26 +0530

    integrate dynamic locations and add ability to create new location

--- frontend/src/components/HotelListings.jsx
+++ frontend/src/components/HotelListings.jsx
@@ -1,16 +1,51 @@
 import HotelCard from "@/components/HotelCard";
-import { hotels, locations } from "@/data";
+import {
+  useGetAllHotelsQuery,
+  useGetAllLocationsQuery,
+  useAddLocationMutation,
+} from "@/lib/api";
+import { Skeleton } from "./ui/skeleton";
 import { useState } from "react";
 import LocationTab from "./LocationTab";
+import { Button } from "./ui/button";
+import { PlusCircle } from "lucide-react";
+import { toast } from "sonner";
 
 function HotelListings() {
   const [selectedLocation, setSelectedLocation] = useState(0);
+
+  const {
+    data: hotels,
+    isLoading: isHotelsLoading,
+    isError: isHotelsError,
+    error: hotelsError,
+  } = useGetAllHotelsQuery();
+
+  const {
+    data: locations,
+    isLoading: isLocationsLoading,
+    isError: isLocationsError,
+    error: locationsError,
+  } = useGetAllLocationsQuery();
+
+  const [
+    addLocation,
+    {
+      isLoading: isAddLocationLoading,
+      isError: isAddLocationError,
+      error: addLocationError,
+    },
+  ] = useAddLocationMutation();
+
+  const allLocations = locations
+    ? [{ _id: 0, name: "All" }, ...locations]
+    : [{ _id: 0, name: "All" }];
 
   const handleLocationSelect = (selectedLocation) => {
     setSelectedLocation(selectedLocation._id);
   };
 
-  const selectedLocationName = locations.find(
+  const selectedLocationName = allLocations.find(
     (el) => selectedLocation === el._id
   ).name;
 
@@ -20,6 +55,57 @@ function HotelListings() {
       : hotels.filter((hotel) => {
           return hotel.location.includes(selectedLocationName);
         });
+
+  const isLoading = isHotelsLoading || isLocationsLoading;
+  const isError = isHotelsError || isLocationsError;
+  const error = [hotelsError, locationsError];
+
+  const handleAddLocation = async () => {
+    try {
+      toast.loading("Adding location...");
+      await addLocation({ name: "Fiji" }).unwrap();
+      toast.success("Location added successfully");
+    } catch (error) {
+      toast.error("Failed to add location");
+    }
+  };
+
+  if (isLoading) {
+    return (
+      <section className="px-8 py-8 lg:py-8">
+        <div className="mb-12">
+          <h2 className="text-3xl md:text-4xl font-bold mb-4">
+            Top trending hotels worldwide
+          </h2>
+          <p className="text-lg text-muted-foreground">
+            Discover the most trending hotels worldwide for an unforgettable
+            experience.
+          </p>
+        </div>
+
+        <Skeleton className="h-6 flex items-center flex-wrap gap-x-4" />
+
+        <Skeleton className="h-24 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 mt-4" />
+      </section>
+    );
+  }
+
+  if (isError) {
+    return (
+      <section className="px-8 py-8 lg:py-8">
+        <div className="mb-12">
+          <h2 className="text-3xl md:text-4xl font-bold mb-4">
+            Top trending hotels worldwide
+          </h2>
+          <p className="text-lg text-muted-foreground">
+            Discover the most trending hotels worldwide for an unforgettable
+            experience.
+          </p>
+        </div>
+        <p className="text-red-500">Error loading data </p>
+      </section>
+    );
+  }
 
   return (
     <section className="px-8 py-8 lg:py-8">
@@ -31,10 +117,17 @@ function HotelListings() {
           Discover the most trending hotels worldwide for an unforgettable
           experience.
         </p>
+        <Button
+          disabled={isAddLocationLoading}
+          className={`${isAddLocationLoading ? "opacity-50" : ""}`}
+          onClick={handleAddLocation}
+        >
+          <PlusCircle className="w-4 h-4" /> Add Location
+        </Button>
       </div>
 
       <div className="flex items-center flex-wrap gap-x-4">
-        {locations.map((location) => {
+        {allLocations.map((location) => {
           return (
             <LocationTab
               onClick={handleLocationSelect}
