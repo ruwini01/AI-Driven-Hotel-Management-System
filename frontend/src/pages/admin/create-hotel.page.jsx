6a75d7da3e5a4521c5deb42ca9faf726fccd20bb
ruwini01 <12345tharanga12345@gmail.com>
2025-10-21 12:46:16 +0530

    added hotel creation page under admin access

---
+++ frontend/src/pages/admin/create-hotel.page.jsx
@@ -0,0 +1,13 @@
+import HotelCreateFrom from "@/components/HotelCreateFrom";
+
+function CreateHotelPage() {
+
+  return (
+    <main className="px-4 py-8">
+      <h1 className="text-3xl font-bold">Create Hotel</h1>
+      <HotelCreateFrom />
+    </main>
+  );
+}
+
+export default CreateHotelPage;
